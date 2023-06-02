5e5b3dcae1cc214527124c24a62dd58d554736ed
Alexey <alexeyzakis@gmail.com>
2023-06-02 18:43:51 +0300

    Added images

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include "libraries/GeneticAlgorithm/geneticAlgorithm.h"
 
 int main() {
-//    player();
+    player();
 //    testAI(10);
-    AI(true, scoreParams, 0);
+//    AI(true, scoreParams, 0);
 //    trainAI();
     return 0;
 }
